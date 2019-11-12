58c5dd5af58125bcc3f8a2107ec85d01b56364ca
edexy <edoja2012@gmail.com>
2019-11-12 23:56:53 +0100

    chore(changes-package-json): changes packages json file [Finishes #1570598]

--- db/connection.js
+++ db/connection.js
@@ -23,12 +23,12 @@ const createUserTable = () => {
         last_name  VARCHAR(255)  NOT NULL,
         email VARCHAR(255)  NOT NULL,
         password VARCHAR(255) NOT NULL,
-        department VARCHAR(255)  NOT NULL,
-        job_role VARCHAR(255)  NOT NULL,
-        gender VARCHAR(255)  NOT NULL,
-        address VARCHAR(255)  NOT NULL,
+        department VARCHAR(255)   NULL,
+        job_role VARCHAR(255)   NULL,
+        gender VARCHAR(255)   NULL,
+        address VARCHAR(255)   NULL,
         user_type INT   NULL,
-        created_at VARCHAR(255),
+        created_at VARCHAR(255) NULL,
         updated_at VARCHAR(255) NULL
       )`;
 
