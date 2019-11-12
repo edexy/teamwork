27256b36b5d56062eec8ed636c53b13c63c6db99
edexy <edoja2012@gmail.com>
2019-11-12 22:44:16 +0100

    chore(changes-package-json): changes packages json file [Finishes #1570598]

--- db/connection.js
+++ db/connection.js
@@ -14,88 +14,88 @@ pool.on('connect', () => {
 /**
  * Create User Table
  */
-const createUserTable = () => {
-    const queryText =
-        `CREATE TABLE IF NOT EXISTS
-      users(
-        id SERIAL PRIMARY KEY,
-        first_name VARCHAR(255)  NOT NULL,
-        last_name  VARCHAR(255)  NOT NULL,
-        email VARCHAR(255)  NOT NULL,
-        password VARCHAR(255) NOT NULL,
-        phone VARCHAR(255)  NOT NULL,
-        deparment VARCHAR(255)  NOT NULL,
-        job_role VARCHAR(255)  NOT NULL,
-        gender VARCHAR(255)  NOT NULL,
-        user_type INT   NULL,
-        created_at VARCHAR(255),
-        updated_at VARCHAR(255)
-      )`;
+// const createUserTable = () => {
+//     const queryText =
+//         `CREATE TABLE IF NOT EXISTS
+//       users(
+//         id SERIAL PRIMARY KEY,
+//         first_name VARCHAR(255)  NOT NULL,
+//         last_name  VARCHAR(255)  NOT NULL,
+//         email VARCHAR(255)  NOT NULL,
+//         password VARCHAR(255) NOT NULL,
+//         phone VARCHAR(255)  NOT NULL,
+//         deparment VARCHAR(255)  NOT NULL,
+//         job_role VARCHAR(255)  NOT NULL,
+//         gender VARCHAR(255)  NOT NULL,
+//         user_type INT   NULL,
+//         created_at VARCHAR(255),
+//         updated_at VARCHAR(255)
+//       )`;
 
-    pool.query(queryText)
-        .then((res) => {
-            console.log(res);
-            pool.end();
-        })
-        .catch((err) => {
-            console.log(err);
-            pool.end();
-        });
-}
+//     pool.query(queryText)
+//         .then((res) => {
+//             console.log(res);
+//             pool.end();
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//             pool.end();
+//         });
+// }
 
 /**
  * Create A Table
  */
-const createGifTable = () => {
-    const queryText =
-        `CREATE TABLE IF NOT EXISTS
-        gifs(
-          id SERIAL PRIMARY KEY,
-          title VARCHAR(255)  NOT NULL,
-          url VARCHAR(255)  NOT NULL,
-          public_id VARCHAR(255)  NOT NULL,
-          user_id VARCHAR(255)  NOT NULL,
-          created_at VARCHAR(255)  NOT NULL,
-          updated_at VARCHAR(255)   NULL
-        )`;
+// const createGifTable = () => {
+//     const queryText =
+//         `CREATE TABLE IF NOT EXISTS
+//         gifs(
+//           id SERIAL PRIMARY KEY,
+//           title VARCHAR(255)  NOT NULL,
+//           url VARCHAR(255)  NOT NULL,
+//           public_id VARCHAR(255)  NOT NULL,
+//           user_id VARCHAR(255)  NOT NULL,
+//           created_at VARCHAR(255)  NOT NULL,
+//           updated_at VARCHAR(255)   NULL
+//         )`;
 
-    pool.query(queryText)
-        .then((res) => {
-            console.log(res);
-            pool.end();
-        })
-        .catch((err) => {
-            console.log(err);
-            pool.end();
-        });
-}
+//     pool.query(queryText)
+//         .then((res) => {
+//             console.log(res);
+//             pool.end();
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//             pool.end();
+//         });
+// }
 
 /**
 * Create A Table
 */
-const createArticleTable = () => {
-    const queryText =
-        `CREATE TABLE IF NOT EXISTS
-        articles(
-          id SERIAL PRIMARY KEY,
-          title VARCHAR(255)  NOT NULL,
-          article VARCHAR(255)  NOT NULL,
-          public_id VARCHAR(255)  NOT NULL,
-          user_id VARCHAR(255)  NOT NULL,
-          created_at VARCHAR(255)  NOT NULL,
-          updated_at VARCHAR(255)   NULL
-        )`;
+// const createArticleTable = () => {
+//     const queryText =
+//         `CREATE TABLE IF NOT EXISTS
+//         articles(
+//           id SERIAL PRIMARY KEY,
+//           title VARCHAR(255)  NOT NULL,
+//           article VARCHAR(255)  NOT NULL,
+//           public_id VARCHAR(255)  NOT NULL,
+//           user_id VARCHAR(255)  NOT NULL,
+//           created_at VARCHAR(255)  NOT NULL,
+//           updated_at VARCHAR(255)   NULL
+//         )`;
 
-    pool.query(queryText)
-        .then((res) => {
-            console.log(res);
-            pool.end();
-        })
-        .catch((err) => {
-            console.log(err);
-            pool.end();
-        });
-}
+//     pool.query(queryText)
+//         .then((res) => {
+//             console.log(res);
+//             pool.end();
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//             pool.end();
+//         });
+// }
 
 /**
 * Create A Table
@@ -128,9 +128,6 @@ const createCommentTable = () => {
  * Create All Tables
  */
 const createAllTables = () => {
-    createUserTable();
-    createGifTable();
-    createArticleTable();
     createCommentTable();
 }
 
