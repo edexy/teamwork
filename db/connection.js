d860f7d83b4d80d44e2b2b478e8c5a9c60569ff8
edexy <edoja2012@gmail.com>
2019-11-14 20:32:41 +0100

    chore(create-test): create test for available endpoints [Start #00001]

--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,7 @@
 // db.js
 const { Pool } = require('pg');
 
+let connectionString = '';
 
 if(process.env.NODE_ENV === 'test'){
     connectionString =  `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
