ad6760843962a1a0e7cfcb52fa08e240095d95be
edexy <edoja2012@gmail.com>
2019-11-26 16:15:16 +0100

    bugfix(feed-endpoint): fix query bug in feed endpoint [Finishes #0001]

--- v1/controllers/feed.js
+++ v1/controllers/feed.js
@@ -1,57 +1,33 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const pool = require('../../db/connection').pool;
 
 
 exports.getFeed = (req, res, next) => {
-    let email = req.body.email;
+    let articles = {};
+    let gifs = {};
+
     pool.connect((err, client, done) => {
-        client.query('(SELECT * FROM articles) UNION ALL (SELECT * FROM gifs)', (error, result) => {
+
+        client.query('SELECT id, title, article, user_id as authorId, created_at as createdOn FROM articles ORDER BY id DESC').then((result) => {
             done();
-            if (result.rowCount < 1) {
-                return res.status(404).json({
-                    status: 'failed',
-                    message: 'Feeds found'
+            client
+                .query('SELECT id, title, url, user_id as authorId, created_at as createdOn FROM gifs ORDER BY id DESC')
+                .then((resp) => {
+
+
+                    return res.status(200).json({
+                        status: 'success',
+                        data: Object.assign({}, resp.rows, result.rows)
+
+
+                    });
+                }).catch(err => {
+                    return res.status(400).json({ err });
                 });
-            }
-
-            if (error) {
-                return res.status(400).json({ error });
-            }
-    
-            res.status(200).json({
-                status: 'success',
-                data: result.rows
-            })
-
-            // bcrypt.compare(req.body.password, result.rows[0].password).then(
-            //     (valid) => {
-            //         if (!valid) {
-            //             return res.status(401).json({
-            //                 status: 'failed',
-            //                 message: 'Incorrect Password'
-            //             });
-            //         }
-            //         const token = jwt.sign({ userId: result.rows[0].id, userType: result.rows[0].user_type },
-            //             'RANDOM_TOKEN_SECRET', { expiresIn: '24h' });
-            //         const resp = {
-            //             token: token,
-            //             UserID:  result.rows[0].id
-            //         };    
-            //         res.status(200).json({
-            //             status: 'success',
-            //             data: resp
-            //         })
-            //     }
-            // ).catch(
-            //     (error) => {
-            //         res.status(500).json({
-            //             error: error
-            //         })
-            //     }
-            // )
-
+        }).catch(error => {
+            return res.status(400).json({ error });
         });
-    });
 
+
+
+    });
 }
